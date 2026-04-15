42d71a83d34d11991e16d39bcd0549554b282a00
Miguel Dovale <mikedovale@pm.me>
2026-04-15 13:21:26 -0700

    queries: refine INI values (number, boolean, hex) via #match?

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -13,5 +13,18 @@
 (setting
   (setting_name) @property)
 
+; Values: generic string first; #match? refinements below override (tree-sitter: later wins).
 (setting
   (setting_value) @string)
+
+((setting_value) @number
+ (#match? @number "^[0-9]+$"))
+
+((setting_value) @number
+ (#match? @number "^[0-9]+\\.[0-9]+$"))
+
+((setting_value) @boolean
+ (#match? @boolean "^(true|false)$"))
+
+((setting_value) @constant.numeric.color
+ (#match? @constant.numeric.color "^#[0-9a-fA-F]{3,8}$"))
